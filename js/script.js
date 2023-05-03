c10e735ddcce9fc8f1e83bd57a388e8e9e4e4495
Galabalab <132389112+Galabalab@users.noreply.github.com>
2023-05-03 14:34:15 +0400

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function myMenu1() {
+	document.getElementById('menu1').style.display='block';
+    document.getElementById('menu2').style.display='none';
+    document.getElementById('menu3').style.display='none';
+}
+
+function myMenu2() {
+	document.getElementById('menu1').style.display='none';
+    document.getElementById('menu2').style.display='block';
+    document.getElementById('menu3').style.display='none';
+}
+
+function myMenu3() {
+	document.getElementById('menu1').style.display='none';
+    document.getElementById('menu2').style.display='none';
+    document.getElementById('menu3').style.display='block';
+}
